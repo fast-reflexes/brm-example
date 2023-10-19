0c8d129c54075d84503dbaaed4db6fba7557a436
Elias Lousseief <elias.lousseief@blackwinged-angel.com>
2023-10-19 20:05:58 +0200

    Initial commit

---
+++ src/app/ExampleMath.tsx
@@ -0,0 +1,12 @@
+import {MathJax} from "better-react-mathjax";
+import React from "react";
+
+const ExampleMath = () => {
+    return (
+        <MathJax>
+            {"$$\\int_{-\\infty}^\\infty e^{-x^2} dx = \\sqrt{\\pi}$$"}
+        </MathJax>
+    );
+};
+
+export { ExampleMath }
