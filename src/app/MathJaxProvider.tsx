0f8f6bf875a0a0527f92f533dc522719c8a17263
Elias Lousseief <elias.lousseief@blackwinged-angel.com>
2023-10-19 20:06:31 +0200

    Initial commit

---
+++ src/app/MathJaxProvider.tsx
@@ -0,0 +1,11 @@
+'use client'
+import React, {PropsWithChildren} from "react";
+import {MathJaxContext} from "better-react-mathjax";
+
+const MathJaxProvider = ({ children }:PropsWithChildren) => (
+    <MathJaxContext src={"/mathjax/tex-chtml.js"}>
+        {children}
+    </MathJaxContext>
+)
+
+export { MathJaxProvider }
